c43b66bc96f9cac0737d9391856185ea5caff3af
Ariana Castro <ariibritez5@gmail.com>
2023-08-31 01:53:44 -0300

    errores modificados en index JS

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ let nombre = prompt ("Ingrese su nombre:");
 alert ("Bienvenido " + nombre);
 let pais = prompt ( "De que país eres ?" );
 
-if ((pais === "Argentina") || (pais =="argentina"));{
+if ((pais === "Argentina") || (pais =="argentina")){
   let edad=  prompt ( "Cuantos años tenes?")
 }
-elseif ( pais != "Argentina") || ( pais != "argentina") ;{
+else if (( pais != "Argentina") || ( pais != "argentina")){
     alert ( "Disculpa no hacemos envios fuera de Argentina")
 }
 
- elseif ( edad >= 18) ;{
+else if ( edad >= 18) {
     alert ( "Ya puedes ingresar a la pagina, Que lo disrutes")
 } 
-elseif (edad < 18); {
+else if (edad < 18) {
     console.log( "Ingreso un menor no puede comprar");
 }
-
-

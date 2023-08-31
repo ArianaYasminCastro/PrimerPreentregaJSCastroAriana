92561471e01e0b2fb73f581f2fdf25dd449dc057
Ariana Castro <ariibritez5@gmail.com>
2023-08-31 01:11:45 -0300

    primer proyecto

---
+++ index.js
@@ -0,0 +1,20 @@
+//entrada
+let nombre = prompt ("Ingrese su nombre:");
+alert ("Bienvenido " + nombre);
+let pais = prompt ( "De que país eres ?" );
+
+if ((pais === "Argentina") || (pais =="argentina"));{
+  let edad=  prompt ( "Cuantos años tenes?")
+}
+elseif ( pais != "Argentina") || ( pais != "argentina") ;{
+    alert ( "Disculpa no hacemos envios fuera de Argentina")
+}
+
+ elseif ( edad >= 18) ;{
+    alert ( "Ya puedes ingresar a la pagina, Que lo disrutes")
+} 
+elseif (edad < 18); {
+    console.log( "Ingreso un menor no puede comprar");
+}
+
+
